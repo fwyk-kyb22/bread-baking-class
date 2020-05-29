795ef81b5253cb6a7f183c49c56a4524240a5177
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-05-29 14:49:38 +0900

    add view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
-  resources :home, only: :index 
+  resources :home, only: :index
   resources :contacts, only: [:new, :create]
+  resources :lesson,  only: :index
+  resources :posts
 end
