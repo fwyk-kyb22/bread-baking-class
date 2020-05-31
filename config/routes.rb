60cbdd2a18ae0684454822e3aff62102341c5c23
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-05-31 17:37:00 +0900

    add controller admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
   resources :articles
-    namespace :admin do
-      resources :articles
-    end
   resources :home, only: :index
   resources :contacts, only: [:new, :create]
   resources :lesson,  only: :index
-  resources :posts
   resources :schedules, only: :index
 end
