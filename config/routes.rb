783242ac92597caf2d863aaeacd37e662fdbdb23
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-23 14:56:38 +0900

    ransack導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   resources :articles
   resources :home, only: :index
   resources :lesson,  only: :index
