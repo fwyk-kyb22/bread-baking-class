e2f54c162838580a550b9c3cfd208f101851adee
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-26 00:32:32 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,10 +7,10 @@ worker_processes 1
 working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/var/run/unicorn.pid"
 
 #ポート番号を指定
-listen  "#{app_path}/tmp/sockets/unicorn.sock"
+listen  "#{app_path}/var/run/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
