cb04fa1493ce05ba13b9a476658d916037fa3bee
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-25 23:27:39 +0900

    修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,11 @@
-class CardsController < ApplicationController
-  def pay
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    charge = Payjp::Charge.create(
-      amount: 300,
-      card: params['payjp-token'],
-      currency: 'jpy'
-      )
-      redirect_to action: :done
-  end
-end
+# class CardsController < ApplicationController
+#   def pay
+#     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+#     charge = Payjp::Charge.create(
+#       amount: 300,
+#       card: params['payjp-token'],
+#       currency: 'jpy'
+#       )
+#       redirect_to action: :done
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
-  resources :cards, only: [:index] do
-    collection do
-      post 'pay', to: 'cards#pay'
-    end
-  end
+  # resources :cards, only: [:index] do
+  #   collection do
+  #     post 'pay', to: 'cards#pay'
+  #   end
+  # end
   resources :articles
   resources :home, only: :index
   resources :lesson,  only: :index
