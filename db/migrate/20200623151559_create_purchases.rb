b2ca59c2836ed3ca3f766442ca0305340c3349be
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-25 23:33:08 +0900

    修正

--- db/migrate/20200623151559_create_purchases.rb
+++
@@ -1,8 +0,0 @@
-class CreatePurchases < ActiveRecord::Migration[6.0]
-  def change
-    create_table :purchases do |t|
-      t.integer :amount, null: false
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_151559) do
+ActiveRecord::Schema.define(version: 2020_06_23_021040) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,14 +57,6 @@ ActiveRecord::Schema.define(version: 2020_06_23_151559) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
@@ -77,12 +69,6 @@ ActiveRecord::Schema.define(version: 2020_06_23_151559) do
     t.string "title"
     t.text "text"
     t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
