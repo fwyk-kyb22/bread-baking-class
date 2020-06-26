7899b97b6354b4744ec416f3be8449cd5fd9b9df
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-26 23:57:08 +0900

    bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,14 @@
 const { environment } = require('@rails/webpacker')
-var webpack = require('webpack');
 
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-      jQuery: 'jquery/src/jquery',
-      Toastr: 'toastr'
-      Popper: ['popper.js', 'default']
-  })
-)
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default'],
+  Toastr: 'toastr'
+}))
 
 module.exports = environment
+
+
