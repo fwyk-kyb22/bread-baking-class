acaaa0ff26fef46329f91994e7d16d1965036331
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-07-02 02:07:03 +0900

    content追加

--- app/javascript/packs/meetings.js
+++ app/javascript/packs/meetings.js
@@ -3,24 +3,20 @@
 //   toastr.success("hello")
 // });
 $(function(){
-  
+
   $(".day a").hide(); 
-  $(".day a").hide(); 
+  $(".day .meeting__container--content").hide(); 
 
   $(".day").hover(
     function () {
       $(this).find('a').show();
+      $(this).css("background-color","#bfd8d8");
+      $(this).find('.meeting__container--content').show();
     },
     function () {
       $(".day a").hide();
-    }
-  );
-  $(".day").hover(
-    function () {
-      $(this).css("background-color","#bfd8d8");
-    },
-    function () {
-      $(this).css("background-color","#ffffff");
+      $(this).css("background-color","");
+      $(".day .meeting__container--content").hide();
     }
   );
 });
--- db/migrate/20200630125559_create_meetings.rb
+++ db/migrate/20200630125559_create_meetings.rb
@@ -3,7 +3,7 @@ class CreateMeetings < ActiveRecord::Migration[6.0]
     create_table :meetings do |t|
       t.string :name
       t.datetime :start_time
-
+      t.text :content
       t.timestamps
     end
   end
