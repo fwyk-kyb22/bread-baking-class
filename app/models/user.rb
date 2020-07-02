7e7e7c84b36a9bdd03b1e98b10eb8eb600e33fff
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-07-02 12:58:14 +0900

    deviseセキュリティー

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable
   has_many :comments       
 
   validates :nickname,:familyname,:firstname,:familyname_kana,:firstname_kana,:phone_number,:PIN,presence: true
