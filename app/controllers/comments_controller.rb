15d767fce397c2b2df516f049210fa273e652e81
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-07-04 16:28:44 +0900

    コメント　削除ボタン

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to meetings_path, method: :get
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
-  # resources :cards, only: [:index] do
-  #   collection do
-  #     post 'pay', to: 'cards#pay'
-  #   end
-  # end
   resources :articles
   resources :home, only: :index
   resources :lesson,  only: :index
   resources :meetings, except: :show
-  resources :comments, only: :create
+  resources :comments, only:[:create, :destroy]
 end
