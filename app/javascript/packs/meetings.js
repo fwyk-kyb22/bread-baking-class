125f6a4367050c7602686f5ea6a3275ad2577f7d
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-08-17 12:25:43 +0900

    修正　js

--- app/javascript/packs/meetings.js
+++ app/javascript/packs/meetings.js
@@ -3,36 +3,36 @@
 $(function(){
 
   $(".day a").hide(); 
-  $(".meeting__container__one .meeting__container--content").hide(); 
   $(".meeting__container").css('overflow-y','hidden');
-  $(".day .main--left__calendar--container").hide(); 
-
+  
   $(".day").hover(
     function () {
-      $(this).css("background-color","#bfd8d8");
       $(this).find('.meeting__new--btn').show();
       $(this).find(".meeting__container").css('overflow-y','visible');
       $(this).find(".meeting__container").css('height','auto');
-      $(this).find('.meeting__container--content').show();
     },
     function () {
-      $(this).css("background-color","");
       $(this).find('.meeting__new--btn').hide();
       $(this).find(".meeting__container").css('overflow-y','hidden');
       $(this).find(".meeting__container").css('height','80px');
-      $(this).find('.meeting__container--content').hide();
     }
   );
   $(".meeting__container__one").hover(
     function () {
-      $(this).find('.meeting__container--content').show();
       $(this).find('a').show();
     },
     function () {
-      $(".day .meeting__container--content").hide();
       $(".day a").hide();
     }
   );
+  $('.details_btn').on('click',function(){
+    $(this).find('.popup').show();
+  });
+
+  $('.popup__bg').on('click',function(){
+    $(this).parent().fadeOut();
+  });
+  
 
   $(".day .main--left__calendar--container").hide(); 
   $('.day .main--left__calendar--mark').show();
