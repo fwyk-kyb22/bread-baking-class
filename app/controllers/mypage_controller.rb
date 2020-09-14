6d5166f42693538fbfb6b76f6c60f22e5efe62cb
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-09-14 23:56:34 +0900

    予約確認表示

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,5 +2,6 @@ class MypageController < ApplicationController
   before_action :authenticate_user!,only: :index
   def index
     @group = Group.find_by(user_id: current_user.id)
+    @meetings = current_user.meetings.order(start_time: "ASC")
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
   has_many :reservations , dependent: :destroy
   has_many :users, through: :reservations
+  def self.today_before
+    where("start_time >= ?",  Date.today)
+  end
 end
