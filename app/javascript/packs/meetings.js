bf21fe2c78e0d190128b8d63b27e3f65386c61a2
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-09-30 16:50:04 +0900

    星css

--- app/javascript/packs/meetings.js
+++ app/javascript/packs/meetings.js
@@ -17,25 +17,34 @@ $(function(){
       $(this).find(".meeting__container").css('height','80px');
     }
   );
-  // $('#star').raty({
-  //   score: 5,
-  //   number:     10,
-  //   size: 36,
-  //   starOff: "#{asset_path('star-off.png')}",
-  //   starOn: "#{asset_path('star-on.png')}",
-  //   starHalf: "#{asset_path('star-half.png')}",
-  //   scoreName: 'meetings[rate]',
-  //   half: true,
-  //   readOnly: false  
-  // });
-  // $("#star").raty(
-  //   {
-  //   score: 4, starType: 'i',
-  //   cancelOff: '/cancel-off.png',
-  //   cancelOn: '/cancel-on.png',
-  //   starHalf: '/star-half.png',
-  //   starOff: '/star-off.png',
-  //   starOn: '/star-on.png'
-  // })
-  // $('#star').raty({ score: 3, path: '/assets/'});
+  $(".rate--1").click(
+    function () {
+      $(this).parent().find(".rate--1").find(".star--on").show();
+      $(this).parent().find(".rate--1").find(".star--off").hide();
+      $(this).parent().find(".rate--2").find(".star--on").hide();
+      $(this).parent().find(".rate--2").find(".star--off").show();
+      $(this).parent().find(".rate--3").find(".star--on").hide();
+      $(this).parent().find(".rate--3").find(".star--off").show();
+    }
+  );
+  $(".rate--2").click(
+    function () {
+      $(this).parent().find(".rate--1").find(".star--on").show();
+      $(this).parent().find(".rate--1").find(".star--off").hide();
+      $(this).parent().find(".rate--2").find(".star--on").show();
+      $(this).parent().find(".rate--2").find(".star--off").hide();
+      $(this).parent().find(".rate--3").find(".star--on").hide();
+      $(this).parent().find(".rate--3").find(".star--off").show();
+    }
+  );
+  $(".rate--3").click(
+    function () {
+      $(this).parent().find(".rate--1").find(".star--on").show();
+      $(this).parent().find(".rate--1").find(".star--off").hide();
+      $(this).parent().find(".rate--2").find(".star--on").show();
+      $(this).parent().find(".rate--2").find(".star--off").hide();
+      $(this).parent().find(".rate--3").find(".star--on").show();
+      $(this).parent().find(".rate--3").find(".star--off").hide();
+    }
+  );
 });
